3fe5eeaea06043b217efbd41290f36ce870d0de7
Eric Odell <eric.odell@ucop.edu>
2019-09-20 18:37:14 -0700

    simple test to see if bucket is created

--- packages/infra/test/infra.test.ts
+++ packages/infra/test/infra.test.ts
@@ -1,6 +1,8 @@
 import { expect as expectCDK, haveResource, SynthUtils } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import Infra = require('../lib/index');
+import s3 = require('../lib/s3');
+import util = require('../lib/util');
 
 test('SQS Queue Created', () => {
     const app = new cdk.App();
@@ -18,4 +20,25 @@ test('SNS Topic Created', () => {
   new Infra.Infra(stack, 'MyTestConstruct');
   // THEN
   expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+});
+
+test('Bucket Created', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  // WHEN
+  const props = 
+        {
+      content: "log",
+      description: "logging",
+      env: "dev",
+      log_bucket_name: "",
+      label: "log",
+      owner: "log",
+      product: "mywidget",
+      security_level: "1",
+      zone: "log"
+    };
+  new s3.Bucket(stack, "MyTestBucket", props);
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
 });
