ff0248119e3d1c6833c4c3ba3c668d04a4a1eb35
Eric Odell <eric.odell@ucop.edu>
2019-09-22 20:50:34 -0700

    remove boiler plate inherited from init

--- packages/infra/test/infra.test.ts
+++ packages/infra/test/infra.test.ts
@@ -1,26 +1,7 @@
 import { expect as expectCDK, haveResource, SynthUtils } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
-import Infra = require('../lib/index');
 import s3 = require('../lib/s3');
 import util = require('../lib/util');
-
-test('SQS Queue Created', () => {
-  const app = new cdk.App();
-  const stack = new cdk.Stack(app, 'TestStack');
-  // WHEN
-  const sqsTestStack = new Infra.Infra(stack, 'MyTestConstruct');
-  // THEN
-  expectCDK(stack).to(haveResource('AWS::SQS::Queue'));
-});
-
-test('SNS Topic Created', () => {
-  const app = new cdk.App();
-  const stack = new cdk.Stack(app, 'TestStack');
-  // WHEN
-  const snsTestStack = new Infra.Infra(stack, 'MyTestConstruct');
-  // THEN
-  expectCDK(stack).to(haveResource('AWS::SNS::Topic'));
-});
 
 test('Bucket Created', () => {
   const app = new cdk.App();
