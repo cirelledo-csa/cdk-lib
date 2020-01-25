53b6fa8d58c1d78c6f7ad67f910fb3057d6b4dd8
Eric Odell <eric.odell@ucop.edu>
2020-01-25 13:29:33 -0800

    mock app to test list of buckets stack

--- packages/infra/app/app.ts
+++ packages/infra/app/app.ts
@@ -3,6 +3,7 @@ import s3 = require('../lib/s3');
 import util = require('../lib/util');
 import params from '../test/config.json';
 import sampleBucket from '../test/sample-bucket.json';
+import { Buckets } from './lib/buckets';
 
 const branchEnv = util.mapBranchToEnvironment().trim();
 
@@ -17,15 +18,33 @@ const baseprops = {
 };
 
 const bucketprops = {
-  content: 'log',
-  description: 'Super Duper Data From the Earth',
-  label: 'logs',
-  log_bucket_name: '',
+  content: 'magma',
+  description: 'Super Hot Data From the Earth',
+  label: 'sample',
+  log_bucket_name: '/dev/null',
   owner: 'Ms Creosote',
   security_level: '1',
-  zone: 'log',
+  zone: 'core',
 };
 
-  const app = new cdk.App();
-  const stack = new util.BaseStack(app, { baseprops});
-  const myBucket = new s3.Bucket(stack,  { baseprops, bucketprops });
+const stackEnv = { account: '012345678910', region: 'us-east-2' };
+
+const myStackProps: util.IBaseStackProps = {
+  env: stackEnv,
+  baseprops: {
+    buildUrl: 'https://google.com',
+    description: 'Stop Plate Tectonics Now',
+    env: branchEnv,
+    label: 'Buckets',
+    owner: 'Mr Creosote',
+    product: 'sptn',
+    source: 'git',
+  },
+};
+
+const app = new cdk.App();
+const stack = new util.BaseStack(app, { baseprops });
+const myBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+      bucketprops.label = "other";
+const myOtherBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+const buckets = new Buckets(app, myStackProps);
---
+++ packages/infra/app/lib/buckets.ts
@@ -0,0 +1,83 @@
+/**
+ * first pass at creating a datalake
+ * using this as a guide https://docs.aws.amazon.com/lake-formation/latest/dg/getting-started-cloudtrail-tutorial.html
+ */
+
+import iam = require("@aws-cdk/aws-iam");
+import s3 = require("@aws-cdk/aws-s3");
+import cdk = require("@aws-cdk/core");
+import util = require('../../lib/util');
+import DataBucket = require('../../lib/s3');
+import params from "../../test/config.json";
+
+export interface ILabeledBucketProps {
+  /**
+   * bucket label, primary key to identify bucket
+   * @attribute
+   */
+  label: string;
+  /**
+   * bucket label, primary key to identify bucket
+   * @attribute
+   */
+  bucket: s3.Bucket;
+}
+
+export interface IBucketHash {
+  [label: string]: s3.Bucket;
+}
+
+export class Buckets extends util.BaseStack {
+  public readonly bucketsArnList: string[] = [];
+  public readonly bucketsArnDict: IBucketHash = {};
+  public readonly labeledBuckets: ILabeledBucketProps[] = [];
+  constructor(scope: cdk.App, props: util.IBaseStackProps) {
+    super(scope, props);
+
+    // resources
+
+    // buckets
+
+    // Create S3 Buckets for the Data Lake
+
+    // product Logging bucket
+
+    const stackprops = {
+      baseprops: props.baseprops,
+      bucketprops: {
+        content: "log",
+        description: "application logging",
+        label: "logs",
+        log_bucket_name: "",
+        owner: props.baseprops.owner,
+        security_level: "1",
+        zone: "log",
+      },
+    };
+
+    const LoggingBucket = new DataBucket.Bucket(this, stackprops);
+
+    // iterate through list of buckets
+
+    const bucketList = params[`buckets`];
+    const bucketDict: IBucketHash = {};
+
+
+    for (const bucket of bucketList) {
+      stackprops.bucketprops.content = bucket.content;
+      stackprops.bucketprops.description = bucket.description;
+      stackprops.bucketprops.label = bucket.label;
+      stackprops.bucketprops.log_bucket_name = LoggingBucket.bucket.bucketName;
+      stackprops.bucketprops.owner = bucket.owner;
+      stackprops.bucketprops.security_level = bucket.security_level;
+      stackprops.bucketprops.zone = bucket.zone;
+      const lakeBucket = new DataBucket.Bucket(this, stackprops);
+      this.bucketsArnList.push(lakeBucket.bucket.bucketArn);
+      this.labeledBuckets.push({
+        bucket: lakeBucket.bucket,
+        label: bucket[`label`],
+      });
+      bucketDict[bucket[`label`]] = lakeBucket.bucket;
+    }
+  }
+}
