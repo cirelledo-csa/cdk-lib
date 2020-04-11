cecc8e31323ff565e78f67bd81e41a4e76b81ace
Eric Odell <eric.odell@ucop.edu>
2020-04-10 21:32:19 -0700

    allow byo key

--- packages/infra/test/bucket.test.ts
+++ packages/infra/test/bucket.test.ts
@@ -1,5 +1,6 @@
 import { expect as expectCDK, haveResource, SynthUtils } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
+import kms = require('@aws-cdk/aws-kms');
 import s3 = require('../lib/s3');
 import util = require('../lib/util');
 import params from './config.json';
@@ -19,7 +20,7 @@ const baseprops = {
   source: 'git',
 };
 
-const bucketprops = {
+const bucketprops: s3.IBucketProps = {
   content: 'log',
   description: 'Super Duper Data From the Earth',
   label: 'magma',
@@ -36,7 +37,56 @@ test('Bucket Created', () => {
   // WHEN
   const stack = new util.BaseStack(app, { baseprops });
   const myBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myKMSBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myS3ManagedBucket = new s3.Bucket(stack, { baseprops, bucketprops });
   // THEN
   expectCDK(stack).to(haveResource('AWS::S3::Bucket'));
-  expectCDK(stack).toMatch(sampleBucket.junk4);
+  expectCDK(stack).toMatch(sampleBucket.default);
+});
+
+test('S3 Managed encrypted Bucket Created', () => {
+  const app = new cdk.App();
+  util.tagApp(app, { baseprops });
+  const stackName = util.makeStackName(baseprops);
+  // WHEN
+  const stack = new util.BaseStack(app, { baseprops });
+  bucketprops.encryption = 'S3_MANAGED';
+  const myBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myKMSBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myS3ManagedBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // THEN
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket'));
+  expectCDK(stack).toMatch(sampleBucket.s3managed);
+});
+
+test('KMS encrypted Bucket Created', () => {
+  const app = new cdk.App();
+  util.tagApp(app, { baseprops });
+  const stackName = util.makeStackName(baseprops);
+  // WHEN
+  const stack = new util.BaseStack(app, { baseprops });
+  bucketprops.encryption = 'KMS';
+  const myBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myKMSBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myS3ManagedBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // THEN
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket'));
+  expectCDK(stack).toMatch(sampleBucket.kms);
+});
+
+test('BYO KMS encrypted Bucket Created', () => {
+  const app = new cdk.App();
+  util.tagApp(app, { baseprops });
+  const stackName = util.makeStackName(baseprops);
+  // WHEN
+  const stack = new util.BaseStack(app, { baseprops });
+  const myKey = new kms.Key(stack, 'MyKey');
+  bucketprops.encryption = 'KMS';
+  bucketprops.encryptionKey = myKey;
+  const myBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myKMSBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // const myS3ManagedBucket = new s3.Bucket(stack, { baseprops, bucketprops });
+  // THEN
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket'));
+  expectCDK(stack).toMatch(sampleBucket.byokey);
 });
